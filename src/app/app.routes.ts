3b304e4e459db8d24ae15b8a4342464613fd950f
shahab attarnejad <atarnezhad@hotmail.com>
2024-06-02 13:20:30 +0330

    Implementing the router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./about/about.component').then((c) => c.AboutComponent),
+  },
+];
